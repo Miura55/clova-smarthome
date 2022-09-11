91ef1110612188f42f29046010def63a1db6088d
Miura55 <acordion.piano@gmail.com>
2022-09-11 15:47:07 +0900

    add load env and set function urls

--- cdk/lib/lambda-container-stack.ts
+++ cdk/lib/lambda-container-stack.ts
@@ -1,14 +1,35 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
+import {
+  DockerImageFunction,
+  DockerImageCode,
+  FunctionUrlAuthType,
+  HttpMethod,
+} from 'aws-cdk-lib/aws-lambda';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '../.env' });
 
 export class ClovaIoThubStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    new lambda.DockerImageFunction(this, 'clova-iot-hub', {
-      code: lambda.DockerImageCode.fromImageAsset('../lambda'),
-    })
+    const funcionApp = new DockerImageFunction(this, 'clova-iot-hub', {
+      code: DockerImageCode.fromImageAsset('../lambda'),
+      environment: {
+        'BEEBOTTE_URL': process.env.BEEBOTTE_URL || '',
+        'CLOVA_EXTENSION_ID': process.env.CLOVA_EXTENSION_ID || '',
+      }
+    });
+
+    const funcitonUrl = funcionApp.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedMethods: [HttpMethod.ALL],
+        allowedOrigins: ['*'],
+      } 
+    });
+    new CfnOutput(this, 'FunctionUrl', { value: funcitonUrl.url });
   }
 }
